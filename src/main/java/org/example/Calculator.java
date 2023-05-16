29e1b834ff3ba81595b365d504dabb1af0cedeac
Ivan <ivan@MacBook.local>
2023-05-16 10:09:43 +0300

    calulation moved to return

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -4,29 +4,23 @@ public class Calculator {
     static double x, y;
 
     public static double div (){
-        double result;
 
         if (y == 0) {
             throw new RuntimeException("Division by zero is not allowed");
-            //System.out.println("divding on 0 is forbiden");
 
         }
         return x / y;
     }
 
     public static double add (){
-        double result;
-        result = x + y;
-        return result;
+        return x + y;
     }
 
     public static double sub (){
-        double result;
         return x - y;
     }
 
     public static double mult (){
-        double result;
         return x * y;
     }
 
